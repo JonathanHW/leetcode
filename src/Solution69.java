7099efb4ba3de505ffea2fc07c6520bbc35fdeb6
wuhao1_ext <wuhao1_ext@cop.cmsoft.com.cn>
2023-02-14 13:16:07 +0800

    69题目描述

--- src/Solution69.java
+++ src/Solution69.java
@@ -1,3 +1,9 @@
+/**
+ * 给定一个正整数，求该数的平方根，只需求最解决的正整数解。
+ * （原69题目改编，原题输出不保留小数的平方根）
+ * 例：输入8 输出3
+ *    输入21 输出5
+ */
 public class Solution69 {
     public static void main(String[] args) {
         System.out.println(sqrt(21));
@@ -16,6 +22,7 @@ public class Solution69 {
             int mid = (left + right + 1) >>> 1;
             int sq = mid * mid;
             if (sq > x) {
+                //原69题目改编，原题输出不保留小数的平方根
                 if (sq - x < x - (mid - 1) * (mid - 1)) {
                     return mid;
                 }
